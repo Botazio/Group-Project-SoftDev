3a70514948be7930822053895bd47774ff16c209
Alvaro Garcia <alvaro.garcia@ucdconnect.ie>
2021-02-20 12:36:17 +0100

    Complete virtual environment

--- webscraping/main.py
+++ webscraping/main.py
@@ -16,7 +16,7 @@ from IPython.display import display
 import json
 
 # Opening JSON file
-f = open('webscraping\passwords.json',)
+f = open(r'webscraping\passwords.json',)
 
 # returns JSON object as a dictionary
 data = json.load(f)
