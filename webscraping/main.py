6b521cd4c2a6f718b2fd44787b269eb58ca9893a
Alvaro Garcia <alvaro.garcia@ucdconnect.ie>
2021-02-20 13:22:07 +0100

    creation of a csv file to store static data as a backup

--- webscraping/main.py
+++ webscraping/main.py
@@ -14,14 +14,13 @@ import requests
 import time
 from IPython.display import display
 import json
+import csv
 
 # Opening JSON file
 f = open(r'webscraping\passwords.json',)
 
 # returns JSON object as a dictionary
 data = json.load(f)
-
-print()
 
 # Closing file
 f.close()
@@ -41,9 +40,8 @@ APIKEY = data["api.key"]
 engine = create_engine(
     "mysql+mysqldb://{}:{}@{}:{}/{}".format(USER, PASSWORD, URL, PORT, DB), echo=True)
 
-# Inject the dinamic info into the database
 
-
+# Inject the dinamic info into the database and a csv file
 def write_to_db(text):
     stations = json.loads(text)
     for station in stations:
@@ -51,11 +49,18 @@ def write_to_db(text):
                 station.get("available_bike_stands"), station.get("last_update"))
         engine.execute(
             "insert into availability (number, available_bikes, available_bikes_stands, last_update) values(%s,%s,%s,%s)", vals)
+        with open(r'webscraping\static_data.csv', mode='a') as csv_file:
+            fieldnames = ['number', 'available_bikes',
+                          'available_bikes_stands', 'last_update']
+            writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
+
+            writer.writerow({'number': vals[0], 'available_bikes': vals[1],
+                             'available_bikes_stands': vals[2], 'last_update': vals[3]})
+    csv_file.close()
     return
 
 
 def main():
-
     while True:
         try:
             r = requests.get(STATIONS_URL, params={
